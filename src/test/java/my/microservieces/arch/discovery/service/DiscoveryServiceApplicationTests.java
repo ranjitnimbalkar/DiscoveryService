30c3360302136a7a346cd226a1ff8609dda5d6eb
Ranjit Nimblakar <ranjitraje.cse@gmail.com>
2021-08-18 08:16:36 +0530

    initial commit

---
+++ src/test/java/my/microservieces/arch/discovery/service/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package my.microservieces.arch.discovery.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
